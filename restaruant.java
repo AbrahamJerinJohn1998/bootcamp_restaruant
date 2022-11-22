ded1477c2ccbb75671f8eb7f339f44186a1c1e90
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-22 12:09:50 +0530

    add count

--- restaruant.java
+++ restaruant.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class restaruant {
@@ -6,15 +7,25 @@ public class restaruant {
     int snacks=10;
     int idli=8;
     int dosa=6;
+    int teacount,coffecount,snackscount,idlicount,dosacount;
+    String name;
+    long phno;
 
-    public restaruant(int tea, int coffee, int snacks, int idli, int dosa) {
-        this.tea = tea;
-        this.coffee = coffee;
-        this.snacks = snacks;
-        this.idli = idli;
-        this.dosa = dosa;
+    public restaruant(String name, long phno, int teacount, int coffeecount, int snackscount, int idlicount, int dosacount) {
+        this.name=name;
+        this.phno=phno;
+        this.teacount = teacount;
+        this.coffecount = coffeecount;
+        this.snackscount = snackscount;
+        this.idlicount = idlicount;
+        this.dosacount = dosacount;
+
+
     }
-
+public int totalprice()
+{
+    return (teacount*tea)+(coffecount*coffee)+(snackscount*snacks)+(idlicount*idli)+(dosacount*dosa);
+}
     public int getTea() {
         return tea;
     }
@@ -57,22 +68,47 @@ public class restaruant {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        ArrayList<restaruant> arr = new ArrayList<restaruant>();
         System.out.println("Enter 1-Add count\n2-Generate bill\n3-view all transactions\n4-exit");
         int choice= sc.nextInt();
-        switch (choice)
-        {
-            case 1:
-                System.out.println("Enter order details: ");
-                break;
-            case 2:
-                System.out.println("Your bill");
-                break;
-            case 3:
-                System.out.println("All transactions");
-                break;
-            case 4:System.exit(0);
-            default:
-                System.out.println("Invalid invoice");
+        int a=0;
+        while (a!=1) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter order details: ");
+                    System.out.println("Enter customer name: ");
+                    String name= sc.next();
+                    System.out.println("Enter customer phone no: ");
+                    long phno= sc.nextLong();
+                    System.out.println("Enter count");
+                    System.out.println("Tea: ");
+                    int teacount = sc.nextInt();
+                    System.out.println("Coffee: ");
+                    int coffeecount = sc.nextInt();
+                    System.out.println("Snacks: ");
+                    int snackscount = sc.nextInt();
+                    System.out.println("Idli: ");
+                    int idlicount = sc.nextInt();
+                    System.out.println("Dosa: ");
+                    int dosacount = sc.nextInt();
+                    restaruant obj=new restaruant(name,phno,teacount,coffeecount,snackscount,idlicount,dosacount);
+                    int tprice= obj.totalprice();
+
+                    break;
+                case 2:
+                    System.out.println("Your bill");
+
+                    break;
+                case 3:
+                    System.out.println("All transactions");
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid invoice");
+            }
+            System.out.println("Enter 0 to continue or 1 to exit");
+            a= sc.nextInt();
         }
     }
 }
